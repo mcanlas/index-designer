e2e3a6893d889c458a8a3f57f606047c38ae0b72
Mark Canlas <github@htmlism.com>
2023-10-04 17:09:53 -0400

    improve arity

--- alexandria-core/src/main/scala/com/htmlism/alexandria/dynamodb/Table.scala
+++ alexandria-core/src/main/scala/com/htmlism/alexandria/dynamodb/Table.scala
@@ -4,4 +4,4 @@ object Table:
   enum PrimaryKey:
     case Simple(attribute: String)
     case Composite(partitionKey: String, sortKey: String)
-    case SyntheticComposite(partitionKey: String, sortKey: String*)
+    case SyntheticComposite(partitionKey: String, first: String, second: String, xs: String*)
