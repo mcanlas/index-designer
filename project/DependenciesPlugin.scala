2ce46269104d3c40de8aa13945b99b65f5e170d5
Mark Canlas <github@htmlism.com>
2025-03-14 16:18:45 -0400

    cats-effect 3.6.0-RC2 :space_invader:

--- project/DependenciesPlugin.scala
+++ project/DependenciesPlugin.scala
@@ -8,7 +8,7 @@ object DependenciesPlugin extends AutoPlugin {
     implicit class DependencyOps(p: Project) {
       def withEffectMonad: Project =
         p
-          .settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.5.7")
+          .settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.6.0-RC2")
 
       def withTesting: Project = {
         val weaverVersion =
